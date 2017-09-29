7d3ab4959d81d30ef0419369b122a3b2d87d539f
nwy140 <nwy140@gmail.com>
2017-09-29 22:21:53 +0800

    Update README.c

--- README.c
+++ README.c
@@ -4,20 +4,20 @@ My explanations of the fudenmentals of coding
 
 
 # Pointers
-Pointers are addresses of memories that points to a variable which stores data. <br>
-//---create pointers                              <br>
-int k=5;                                          <br>
-int *pk=k; // (create pointers, pk points to k)   <br>
-  //--Memory Diagram                              <br>
-  |5| <- ||                                       <br>
-  k      pk                                       <br>
-  //--Memory Diagram--                            <br>
-  //-explanation                                  <br>
-   a pointer pk pointing to k is created , pk stores the address of k ,   <br> 
-   since k  is  initialised to 5, pointer pk whenever called will go to k, and grab whatever k is currently = to;   <br>
+Pointers are addresses of memories that points to a variable which stores data. 
+//---create pointers                              
+int k=5;                                          
+int *pk=k; // (create pointers, pk points to k)   
+  //--Memory Diagram                              
+  |5| <- ||                                       
+  k      pk                                       
+  //--Memory Diagram--                           
+  //-explanation                                 
+   a pointer pk pointing to k is created , pk stores the address of k , 
+   since k  is  initialised to 5, pointer pk whenever called will go to k, and grab whatever k is currently = to;  
    
-  //-explanation-                                  <br>
+  //-explanation-
 
   
-//---create pointers---                            <br>
+//---create pointers---                            
 
