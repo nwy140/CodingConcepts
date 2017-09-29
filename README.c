01682ce79502c84991ed8034c88ea1b4efab0a74
nwy140 <nwy140@gmail.com>
2017-09-29 22:45:29 +0800

    Update README.c
    Fixing incorrect explanations

--- README.c
+++ README.c
@@ -5,9 +5,11 @@ My explanations of the fudenmentals of coding
 
 # Pointers
 Pointers are addresses of memories that points to a variable which stores data. 
-//---create pointers                              
-int k=5;                                          
-int *pk=k; // (create pointers, pk points to k)   
+//---create pointers 
+int *pk; // create new pointer,to avoid bugs , initialise pointers to null
+int k; // allocate new memory             
+pk = &k;  // pk points to k
+*pk=5; //  deference 5, in other words, put the value 5 in wherever p is pointing to , which in this case its pointing to k
   //--Memory Diagram                              
   |5| <- ||                                       
   k      pk                                       
@@ -18,13 +20,15 @@ int *pk=k; // (create pointers, pk points to k)
   //-explanation-  
 //---create pointers---                            
 
-//---creating memory
+//---creating memory and changing where pointer is pointing to 
 int m=4; // 1 byte is allocated to store integer 4 in m, which uses a new address,
+pk=&m;   // pk now points to m rather than k
   //--Memory Diagram 
-  |4|
-  m
-  |5| <- ||                                       
+  |4| <-  ^
+  m       |
+  |5|    ||                                       
   k      pk    
   //--Memory Diagram--  
-//---creating memory---
+//---creating memory and changing where pointer is pointing to---
+//---
 
